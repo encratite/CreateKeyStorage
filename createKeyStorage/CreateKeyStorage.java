1efbc99ada30335455c2dff3f2ca27b004c001a8
void <git.email.address@googlemail.com>
2010-07-09 02:20:01 +0200

    The CA is actually supposed to be in a truststore, not the keystore

--- createKeyStorage/CreateKeyStorage.java
+++ createKeyStorage/CreateKeyStorage.java
@@ -5,16 +5,15 @@ import java.io.DataInputStream;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.lang.reflect.Array;
 import java.security.KeyFactory;
 import java.security.KeyStore;
 import java.security.PrivateKey;
 import java.security.cert.Certificate;
+import java.security.cert.CertificateException;
 import java.security.cert.CertificateFactory;
 import java.security.spec.PKCS8EncodedKeySpec;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Iterator;
 import java.util.List;
 
 public class CreateKeyStorage  {
@@ -27,38 +26,26 @@ public class CreateKeyStorage  {
 	}
 
 	public static void main(String[] arguments) {
-		if(arguments.length != 6) {
+		if(arguments.length != 5) {
 			String path = CreateKeyStorage.class.getName();
-			System.out.println("Usage: java " + path + " <JKS file> <CA> <certificate> <key> <alias> <password>");
+			System.out.println("Usage: java " + path + " <JKS file> <certificate> <key> <alias> <password>");
 			return;
 		}
 		
 		String jksPath = arguments[0];
-		String certificateAuthorityPath = arguments[1];
-		String clientCertificatePath = arguments[2];
-		String clientKeyPath = arguments[3];
-		String alias = arguments[4];
-		char[] password = arguments[5].toCharArray();
+		String clientCertificatePath = arguments[1];
+		String clientKeyPath = arguments[2];
+		String alias = arguments[3];
+		char[] password = arguments[4].toCharArray();
 
 		try {
 			byte[] key = readFile(clientKeyPath);
 			KeyFactory keyFactory = KeyFactory.getInstance("RSA");
 			PKCS8EncodedKeySpec keySpecification = new PKCS8EncodedKeySpec(key);
 			PrivateKey privateKey = keyFactory.generatePrivate(keySpecification);
-
-			String[] certificatePaths = {
-				certificateAuthorityPath,
-				clientCertificatePath
-			};
 			
 			List<Certificate> certificates = new ArrayList<Certificate>(); 
-			for(String certificatePath : certificatePaths) {
-				byte[] certificateBytes = readFile(certificatePath);
-				CertificateFactory certificateFactory = CertificateFactory.getInstance("X.509");
-				ByteArrayInputStream certificateStream = new ByteArrayInputStream(certificateBytes);
-				Collection<? extends Certificate> certificateCollection = certificateFactory.generateCertificates(certificateStream);
-				certificates.addAll(certificateCollection);
-			}
+			addCertificate(clientCertificatePath, certificates);
 			Certificate[] certificateArray = certificates.toArray(new Certificate[certificates.size()]);
 
 			KeyStore keyStore = KeyStore.getInstance(KeyStore.getDefaultType());
@@ -68,5 +55,13 @@ public class CreateKeyStorage  {
 		} catch (Exception exception) {
 			exception.printStackTrace();
 		}
+	}
+	
+	private static void addCertificate(String certificatePath, List<Certificate> output) throws CertificateException, IOException {
+		byte[] certificateBytes = readFile(certificatePath);
+		CertificateFactory certificateFactory = CertificateFactory.getInstance("X.509");
+		ByteArrayInputStream certificateStream = new ByteArrayInputStream(certificateBytes);
+		Collection<? extends Certificate> certificateCollection = certificateFactory.generateCertificates(certificateStream);
+		output.addAll(certificateCollection);
 	}
 }
